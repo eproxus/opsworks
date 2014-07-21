62b5732071b6fd6f0a35935ba100f599cdfbc98c
Adam Lindberg <hello@alind.io>
2014-07-21 11:40:36 +0200

    Version 0.0.16
    New features
    * `dsh` command to create DSH configuration files
    * By default, if no stack ID is configured, all stacks are used

--- lib/opsworks/meta.rb
+++ lib/opsworks/meta.rb
@@ -1,7 +1,8 @@
 module OpsWorks
-  VERSION = "0.0.15"
+  VERSION = "0.0.16"
   AUTHORS = ["Adam Lindberg"]
   EMAIL = ["hello@alind.io"]
   DESCRIPTION = "Amazon OpsWorks CLI"
   SUMMARY = "Command line interface for Amazon OpsWorks"
 end
+
